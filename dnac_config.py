6e4ecc529735d131b70a1511e498a89dabab615a
Anne Gentle <agentle@cisco.com>
2019-01-30 11:28:46 +0100

    Update to 443 port value for DevNet Sandbox

--- dnac_config.py
+++ dnac_config.py
@@ -1,5 +1,5 @@
 import os
 DNAC=os.environ.get('DNAC','sandboxdnac.cisco.com')
-DNAC_PORT=os.environ.get('DNAC_PORT',8080)
+DNAC_PORT=os.environ.get('DNAC_PORT',443) # Port value from DevNet Sandbox
 DNAC_USER=os.environ.get('DNAC_USER','devnetuser')
 DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123!')
